8186d6a8050ce719825b70e25cb0285ea7adc92f
NedaudChristophe <christophe.nedaud@outlook.com>
2022-12-14 17:54:42 +0100

    deco

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -13,40 +13,36 @@ class ApiController extends AbstractController
         // on renvoie donc une 404
         $error = new JsonError($message, 404);
 
-        return $this->json(
-            $error,
-            Response::HTTP_NOT_FOUND, // 404
-            // les autres paramètres sont inutiles
-        );
+        return $this->json( $error, Response::HTTP_NOT_FOUND );
+       
     }
 
     public function json200($data, $groups)
     {
-        return $this->json(
-            $data,
-            // code HTTP pour dire que tout se passe bien (200) 
-            Response::HTTP_OK,
-            // les entêtes HTTP, on les utilise dans très peu de cas, donc valeur par défaut : []
-            [],
-            // le contexte, on l'utilise pour spécifier les groupes de serialisation
-            $groups
-        );
-    }
-
-    public function json201($data, string $group)
+        return $this->json($data, Response::HTTP_OK, [], $groups );
+    }        
+            
+            
+     public function json201($data, string $group)
     {
-        return $this->json(
-            $data,
-            201,
-            [],
-            [
-                "groups" =>
-                [
-                    $group
-                ]
-            ]
-        );
-    }
+        return $this->json($data, 201,[], ["groups" => [$group]]);
+    }                 
+            
+           
+            
+    
+
+          
+                
+            
+           
+            
+            
+                
+                
+            
+        
+    
 
 
     public function json422($errors, $data)
