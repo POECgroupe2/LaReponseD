04e39d6a2ce941cb64b004d5a3a9072ef84661e5
cyber-faraday <gilbertofradet@gmail.com>
2022-12-19 11:57:20 +0000

    test2

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,7 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -11,8 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_front_home')]
     public function index(): Response
     {
+
         return $this->render('front/home/index.html.twig', [
             'controller_name' => 'HomeController',
+
         ]);
     }
 }
